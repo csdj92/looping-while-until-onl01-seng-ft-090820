9bd0c04e83bd1613758495b038dce9d583391566
CJ Davis <csdj92@gmail.com>
2020-09-09 12:49:21 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,7 +2,7 @@ def using_while
   levitation_force = 6
 	while levitation_force < 10 
 	levitation_force+=1
-	puts "Its Leviosa not Leviosar, your force is only #{levitation_force}"
+	puts " Its Leviosa not Leviosar, your force is only #{levitation_force}"
  
 end
 end
